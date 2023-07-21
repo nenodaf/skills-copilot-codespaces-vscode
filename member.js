83d7d111fe9860a5c87a7e440a186acd31f378a0
Nawal Daftardar <130915445+nenodaf@users.noreply.github.com>
2023-07-21 22:08:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/member.html',
+    scope: {
+      member: '='
+    },
+    controller: function($scope) {
+      $scope.skills = $scope.member.skills;
+    }
+  };
+}
+
